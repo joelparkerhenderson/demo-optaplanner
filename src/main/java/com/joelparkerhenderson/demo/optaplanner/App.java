950048c07acf611d39cf3c35da4248667ebc1bbb
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-11-01 14:31:45 -0700

    Add final scope

--- src/main/java/com/joelparkerhenderson/demo/optaplanner/App.java
+++ src/main/java/com/joelparkerhenderson/demo/optaplanner/App.java
@@ -12,32 +12,32 @@ public class App
     {
         System.out.println("Demo OptaPlanner version 0.1.0");
 
-        Set<Maker> makers = new HashSet<Maker>();
-        Set<Taker> takers = new HashSet<Taker>();
-        Set<Matcher> matchers = new HashSet<Matcher>();
+        final Set<Maker> makers = new HashSet<Maker>();
+        final Set<Taker> takers = new HashSet<Taker>();
+        final Set<Matcher> matchers = new HashSet<Matcher>();
         
-        Maker makerA = new Maker(); makerA.setName("A"); makers.add(makerA);
-        Maker makerB = new Maker(); makerB.setName("B"); makers.add(makerB);
-        Maker makerC = new Maker(); makerC.setName("C"); makers.add(makerC);
-        Maker makerD = new Maker(); makerD.setName("D"); makers.add(makerD);
-        Maker makerE = new Maker(); makerE.setName("E"); makers.add(makerE);
-        Maker makerF = new Maker(); makerF.setName("F"); makers.add(makerA);
+        final Maker makerA = new Maker(); makerA.setName("A"); makers.add(makerA);
+        final Maker makerB = new Maker(); makerB.setName("B"); makers.add(makerB);
+        final Maker makerC = new Maker(); makerC.setName("C"); makers.add(makerC);
+        final Maker makerD = new Maker(); makerD.setName("D"); makers.add(makerD);
+        final Maker makerE = new Maker(); makerE.setName("E"); makers.add(makerE);
+        final Maker makerF = new Maker(); makerF.setName("F"); makers.add(makerA);
 
-        Taker takerA = new Taker(); takerA.setName("A"); takers.add(takerA);
-        Taker takerB = new Taker(); takerB.setName("B"); takers.add(takerB);
-        Taker takerC = new Taker(); takerC.setName("C"); takers.add(takerC);
-        Taker takerD = new Taker(); takerD.setName("D"); takers.add(takerD);
-        Taker takerE = new Taker(); takerE.setName("E"); takers.add(takerE);
-        Taker takerF = new Taker(); takerF.setName("F"); takers.add(takerF);
+        final Taker takerA = new Taker(); takerA.setName("A"); takers.add(takerA);
+        final Taker takerB = new Taker(); takerB.setName("B"); takers.add(takerB);
+        final Taker takerC = new Taker(); takerC.setName("C"); takers.add(takerC);
+        final Taker takerD = new Taker(); takerD.setName("D"); takers.add(takerD);
+        final Taker takerE = new Taker(); takerE.setName("E"); takers.add(takerE);
+        final Taker takerF = new Taker(); takerF.setName("F"); takers.add(takerF);
 
-        Matcher matcherA = new Matcher(); matcherA.setName("A"); matcherA.setMaker(makerA); matcherA.setTaker(takerA); matchers.add(matcherA);
-        Matcher matcherB = new Matcher(); matcherB.setName("B"); matcherB.setMaker(makerB); matcherB.setTaker(takerB); matchers.add(matcherB);
-        Matcher matcherC = new Matcher(); matcherC.setName("C"); matcherC.setMaker(makerC); matcherC.setTaker(takerC); matchers.add(matcherC);
-        Matcher matcherD = new Matcher(); matcherD.setName("D"); matcherD.setMaker(makerD); matcherD.setTaker(takerD); matchers.add(matcherD);
-        Matcher matcherE = new Matcher(); matcherE.setName("E"); matcherE.setMaker(makerE); matcherE.setTaker(takerE); matchers.add(matcherE);
-        Matcher matcherF = new Matcher(); matcherF.setName("F"); matcherF.setMaker(makerF); matcherF.setTaker(takerF); matchers.add(matcherF);
+        final Matcher matcherA = new Matcher(); matcherA.setName("A"); matcherA.setMaker(makerA); matcherA.setTaker(takerA); matchers.add(matcherA);
+        final Matcher matcherB = new Matcher(); matcherB.setName("B"); matcherB.setMaker(makerB); matcherB.setTaker(takerB); matchers.add(matcherB);
+        final Matcher matcherC = new Matcher(); matcherC.setName("C"); matcherC.setMaker(makerC); matcherC.setTaker(takerC); matchers.add(matcherC);
+        final Matcher matcherD = new Matcher(); matcherD.setName("D"); matcherD.setMaker(makerD); matcherD.setTaker(takerD); matchers.add(matcherD);
+        final Matcher matcherE = new Matcher(); matcherE.setName("E"); matcherE.setMaker(makerE); matcherE.setTaker(takerE); matchers.add(matcherE);
+        final Matcher matcherF = new Matcher(); matcherF.setName("F"); matcherF.setMaker(makerF); matcherF.setTaker(takerF); matchers.add(matcherF);
 
-        Solution solution = new Solution();
+        final Solution solution = new Solution();
         solution.setMakers(makers);
         solution.setTakers(takers);
         solution.setMatchers(matchers);
@@ -46,6 +46,6 @@ public class App
         for(Matcher matcher: matchers){
             System.out.println("Matcher mame:" + matcher.getName() + " maker_name:" + matcher.getMaker().getName() + " taker_name:" + matcher.getTaker().getName());
         }
-        
+
     }
 }
