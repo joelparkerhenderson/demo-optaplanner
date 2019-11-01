e795c3445289aa3ee382aad4a0c3be5457e69c08
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-11-01 10:53:06 -0700

    Add scorer with matcher

--- src/main/java/com/joelparkerhenderson/demo/optaplanner/Scorer.java
+++ src/main/java/com/joelparkerhenderson/demo/optaplanner/Scorer.java
@@ -1,14 +1,37 @@
 package com.joelparkerhenderson.demo.optaplanner;
 
+import java.util.*;
 import org.optaplanner.core.impl.score.director.easy.EasyScoreCalculator;
 import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
 
 public class Scorer implements EasyScoreCalculator<Solution> {
 
     public HardSoftScore calculateScore(Solution solution) {
-        int hardScore = 0;
-        int softScore = 0;
-        return HardSoftScore.valueOf(hardScore, softScore);
+        final HardSoftScore score = HardSoftScore.of(0,0);
+
+        final Set<Maker> makers = solution.getMakers(); assert makers != null : " makers is null";
+        final Set<Taker> takers = solution.getTakers(); assert takers != null : " makers is null";
+        final Set<Matcher> matchers = solution.getMatchers(); assert matchers != null : " matchers is null";
+
+        for(Matcher matcher: matchers){
+        }
+
+        return score;
     }
+
+    public HardSoftScore calculateScoreWithMatcher(Matcher matcher) {
+        HardSoftScore score = HardSoftScore.of(0,0);
+
+        final Maker maker = matcher.getMaker(); assert maker != null : " maker is null";
+        final Taker taker = matcher.getTaker(); assert taker != null : " taker is null"; 
+
+        final String makerName = maker.getName(); assert makerName != null : " makerName is null"; 
+        final String takerName = taker.getName(); assert takerName != null : " takerName is null"; 
+
+        // Example: prefer the maker name equals the taker name
+        score = score.add(HardSoftScore.of(0, (makerName == takerName) ? 1 : -1));
+
+        return score;
+    }       
 
 }
--- src/test/java/com/joelparkerhenderson/demo/optaplanner/ScorerTest.java
+++ src/test/java/com/joelparkerhenderson/demo/optaplanner/ScorerTest.java
@@ -2,17 +2,50 @@ package com.joelparkerhenderson.demo.optaplanner;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import java.util.*;
 import org.optaplanner.core.impl.score.director.easy.EasyScoreCalculator;
 import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
 
 public class ScorerTest 
 {
     @Test
-    public void name()
+    public void calculateScoreWithSolution()
     {
         final Scorer o = new Scorer();
         final Solution solution = new Solution();
+        solution.setMakers(new HashSet<Maker>());
+        solution.setTakers(new HashSet<Taker>());
+        solution.setMatchers(new HashSet<Matcher>());
         final HardSoftScore score = HardSoftScore.of(0,0);
         assertEquals(score, o.calculateScore(solution));
     }
+
+    @Test
+    public void calculateScoreWithMatcherWithMakerNameTakerNameEqual()
+    {
+        final Scorer o = new Scorer();
+        final String makerName = "alpha";
+        final String takerName = "alpha";
+        final Matcher matcher = new Matcher();
+        final Maker maker = new Maker(); maker.setName(makerName); matcher.setMaker(maker);
+        final Taker taker = new Taker(); taker.setName(takerName); matcher.setTaker(taker);
+        final HardSoftScore exp = HardSoftScore.of(0,1);
+        final HardSoftScore act = o.calculateScoreWithMatcher(matcher);
+        assertEquals(exp, act);
+    }
+
+    @Test
+    public void calculateScoreWithMatcherWithMakerNameTakerNameUnequal()
+    {
+        final Scorer o = new Scorer();
+        final String makerName = "alpha";
+        final String takerName = "bravo";
+        final Matcher matcher = new Matcher();
+        final Maker maker = new Maker(); maker.setName(makerName); matcher.setMaker(maker);
+        final Taker taker = new Taker(); taker.setName(takerName); matcher.setTaker(taker);
+        final HardSoftScore exp = HardSoftScore.of(0,-1);
+        final HardSoftScore act = o.calculateScoreWithMatcher(matcher);
+        assertEquals(exp, act);
+    }
+
 }
