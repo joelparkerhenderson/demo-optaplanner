a5d37325635cce56fd091dcdd98a03261a994b0c
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-11-05 14:23:39 -0800

    Refactor TakerSetTest with helpers

--- src/test/java/com/joelparkerhenderson/demo/optaplanner/TakerSetTest.java
+++ src/test/java/com/joelparkerhenderson/demo/optaplanner/TakerSetTest.java
@@ -6,10 +6,33 @@ import java.util.*;
 
 public class TakerSetTest
 {
+
+    protected TakerSet _o() {
+        final TakerSet TakerSet = new TakerSet();
+        return TakerSet;
+    }
+
+    protected TakerSet _takerSet() {
+        final TakerSet TakerSet = new TakerSet();
+        TakerSet.setName("myTakerSet");
+        return TakerSet;
+    }
+    protected Taker _taker() {
+        final Taker Taker = new Taker();
+        Taker.setName("myTaker");
+        return Taker;
+    }
+
+    protected TagSet _tagSet() {
+        final TagSet tagSet = new TagSet();
+        tagSet.setName("myTagSet");
+        return tagSet;
+    }
+    
     @Test
     public void toXMLStringWithDefault()
     {
-        final TakerSet o = new TakerSet();
+        final TakerSet o = _o();
         final String exp =
             "<taker-set/>";
         final String act = o.toXMLString();
@@ -19,9 +42,7 @@ public class TakerSetTest
     @Test
     public void toXMLStringWithTypical()
     {
-        final TakerSet o = new TakerSet();
-        final String name = "myTakerSet";
-        o.setName(name);
+        final TakerSet o = _takerSet();
         final String exp =
             "<taker-set>\n" +
             "  <name>myTakerSet</name>\n" +
@@ -35,26 +56,26 @@ public class TakerSetTest
     {
         final String xml =
             "<taker-set>\n" +
-            "  <name>foo</name>\n" +
+            "  <name>myTakerSet</name>\n" +
             "</taker-set>";
         final TakerSet o = TakerSet.fromXMLString(xml);
-        assertEquals("foo", o.getName());
+        assertEquals("myTakerSet", o.getName());
     }
 
     @Test
     public void name()
     {
-        final TakerSet o = new TakerSet();
-        final String name = "foo";
+        final TakerSet o = _o();
+        final String name = "myTakerSet";
         o.setName(name);
-        assertEquals(name, o.getName());
+        assertSame(name, o.getName());
     }
 
     @Test
     public void tags()
     {
-        final TakerSet o = new TakerSet();
-        final TagSet tagSet = new TagSet();
+        final TakerSet o = _o();
+        final TagSet tagSet =_tagSet();
         o.setTagSet(tagSet);
         assertSame(tagSet, o.getTagSet());
     }
@@ -62,7 +83,7 @@ public class TakerSetTest
     @Test
     public void takers()
     {
-        final TakerSet o = new TakerSet();
+        final TakerSet o = _o();
         final Set<Taker> takers = new HashSet<Taker>();
         o.setTakers(takers);
         assertSame(takers, o.getTakers());
