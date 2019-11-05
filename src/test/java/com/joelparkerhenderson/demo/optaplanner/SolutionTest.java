c63eeabb346d11247ea9a0c49e96c5b9caf9d8d6
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-11-05 14:37:55 -0800

    Refactor SolutionTest with helpers

--- src/test/java/com/joelparkerhenderson/demo/optaplanner/SolutionTest.java
+++ src/test/java/com/joelparkerhenderson/demo/optaplanner/SolutionTest.java
@@ -7,52 +7,88 @@ import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
 
 public class SolutionTest
 {
+    protected Solution _o() {
+        final Solution solution = new Solution();
+        return solution;
+    }
+
+    protected Solution _solution() {
+        final Solution solution = new Solution();
+        solution.setName("mySolution");
+        return solution;
+    }
+
+    protected Matcher _matcher() {
+        final Matcher matcher = new Matcher();
+        matcher.setName("myMatcher");
+        return matcher;
+    }
+
+    protected Maker _maker() {
+        final Maker maker = new Maker();
+        maker.setName("myMaker");
+        return maker;
+    }
+
+    protected Taker _taker() {
+        final Taker taker = new Taker();
+        taker.setName("myTaker");
+        return taker;
+    }
+
+    protected TagSet _tagSet() {
+        final TagSet tagSet = new TagSet();
+        tagSet.setName("myTagSet");
+        return tagSet;
+    }
+
+    protected HardSoftScore _score() {
+        final HardSoftScore score = HardSoftScore.of(123,456);
+        return score;
+    }
+
     @Test
     public void toStringWithDefault()
     {
-        final Solution o = new Solution();
+        final Solution o = _o();
         assertEquals("name:null,score:(0hard/0soft)", o.toString());
     }
 
     @Test
     public void toStringWithTypical()
     {
-        final Solution o = new Solution();
-        final String name = "foo";
-        o.setName(name);
-        final HardSoftScore score = HardSoftScore.of(123,456);
+        final Solution o = _solution();
+        final HardSoftScore score = _score();
         o.setScore(score);
-        assertEquals("name:foo,score:(123hard/456soft)", o.toString());
+        assertEquals("name:mySolution,score:(123hard/456soft)", o.toString());
     }
 
     @Test
     public void toStringDeepWithDefault()
     {
-        final Solution o = new Solution();
+        final Solution o = _o();
         assertEquals("name:null,score:(0hard/0soft),matchers:[]", o.toStringDeep());
     }
 
     @Test
     public void toStringDeepWithTypical()
     {
-        final Solution o = new Solution();
-        o.setName("A");
-        final HardSoftScore score = HardSoftScore.of(123,456);
+        final Solution o = _solution();
+        final HardSoftScore score = _score();
         o.setScore(score);
-        final Matcher matcher = new Matcher();
-        matcher.setName("B");
-        final Maker maker = new Maker(); maker.setName("C"); matcher.setMaker(maker);
-        final Taker taker = new Taker(); taker.setName("D"); matcher.setTaker(taker);
+        final Matcher matcher = _matcher();
+        final Maker maker = _maker(); matcher.setMaker(maker);
+        final Taker taker = _taker(); matcher.setTaker(taker);
         final Set<Matcher> matchers = new HashSet<Matcher>();
         matchers.add(matcher);     
         o.setMatchers(matchers);
-        assertEquals("name:A,score:(123hard/456soft),matchers:[matcher:{name:B,maker:{name:C},taker:{name:D}}]", o.toStringDeep());
+        assertEquals("name:mySolution,score:(123hard/456soft),matchers:[matcher:{name:myMatcher,maker:{name:myMaker},taker:{name:myTaker}}]", o.toStringDeep());
     }
 
     @Test
     public void toXMLStringWithDefault()
     {
-        final Solution o = new Solution();
+        final Solution o = _o();
         final String exp =
             "<solution>\n" +
             "  <score>\n" +
@@ -68,16 +104,12 @@ public class SolutionTest
     @Test
     public void toXMLStringWithTypical()
     {
-        final Solution o = new Solution();
-        final String name = "mySolution";
-        o.setName(name);
-        final Maker maker = new Maker();
-        maker.setName("myMaker");
+        final Solution o = _solution();
+        final Maker maker = _maker();
         final Set<Maker> makers = new HashSet<Maker>();
         makers.add(maker);
         o.setMakers(makers);
-        final Taker taker = new Taker();
-        taker.setName("myTaker");
+        final Taker taker = _taker();
         final Set<Taker> takers = new HashSet<Taker>();
         takers.add(taker);
         o.setTakers(takers);
@@ -109,26 +141,26 @@ public class SolutionTest
     {
         final String xml =
             "<solution>\n" +
-            "  <name>foo</name>\n" +
+            "  <name>mySolution</name>\n" +
             "</solution>";
         final Solution o = Solution.fromXMLString(xml);
-        assertEquals("foo", o.getName());
+        assertEquals("mySolution", o.getName());
     }
 
     @Test
     public void name()
     {
         final Solution o = new Solution();
-        final String name = "foo";
+        final String name = "mySolution";
         o.setName(name);
-        assertEquals(name, o.getName());
+        assertSame(name, o.getName());
     }
 
     @Test
     public void score()
     {
-        final Solution o = new Solution();
-        final HardSoftScore score = HardSoftScore.of(123,456);
+        final Solution o = _o();
+        final HardSoftScore score = _score();
         o.setScore(score);
         assertSame(score, o.getScore());
     }
@@ -136,7 +168,7 @@ public class SolutionTest
     @Test
     public void makers()
     {
-        final Solution o = new Solution();
+        final Solution o = _o();
         final Set<Maker> makers = new HashSet<Maker>();
         o.setMakers(makers);
         assertSame(makers, o.getMakers());
@@ -145,7 +177,7 @@ public class SolutionTest
     @Test
     public void takers()
     {
-        final Solution o = new Solution();
+        final Solution o = _o();
         final Set<Taker> takers = new HashSet<Taker>();
         o.setTakers(takers);
         assertSame(takers, o.getTakers());
@@ -154,7 +186,7 @@ public class SolutionTest
     @Test
     public void matchers()
     {
-        final Solution o = new Solution();
+        final Solution o = _o();
         final Set<Matcher> matchers = new HashSet<Matcher>();
         o.setMatchers(matchers);
         assertSame(matchers, o.getMatchers());
